5ea5231da11fb378c4cdfc9db4f76e13ac221337
Vivek Kumar <110549126+BASU4100@users.noreply.github.com>
2024-07-23 23:11:12 +0530

    Created Rabin-Karp_Algorithm_for_Pattern_Searching.java
    Solution to Geeks for Geeks Problem Rabin-Karp Algorithm for Pattern Searching

---
+++ Rabin-Karp_Algorithm_for_Pattern_Searching.java
@@ -0,0 +1,7 @@
+class Solution {
+  public void findPattern(String s, String p) {
+    int i=0;
+    while (s.indexOf(p,i)!=-1)
+      System.out.println("Pattern found at index "+s.indexOf(p,i++);
+  }
+}
